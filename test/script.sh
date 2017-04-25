edf8ca1767ef6d92b1b03b2a6170db0d0eee4e1d
root <root@play.nim-lang.org>
2017-04-25 15:41:13 +0000

    Fixing permissions issue.

--- test/script.sh
+++ test/script.sh
@@ -5,6 +5,9 @@ file=$1
 exec  1> $"/usercode/logfile.txt"
 exec  2> $"/usercode/errors.txt"
 
+chmod 777 /usercode/logfile.txt
+chmod 777 /usercode/errors.txt
+
 nim c /usercode/$file
 if [ $? -eq 0 ];	then
     ./usercode/${file/.nim/""}
